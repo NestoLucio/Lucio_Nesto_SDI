f6a33c7a399be368bc0950871809f710a07b6f94
NestoLucio <nestolucio@fullsail.edu>
2015-07-09 19:18:16 -0400

    Created starting files for day 2
    Created files in WebStorm.

---
+++ Day 2/js/script.js
@@ -0,0 +1,19 @@
+/*
+This is going to be a multi lined comment
+Nesto Lucio 7-9-15
+SDI Section 00
+Day 2 Coding Basics.
+ */
+
+//alert("This is making sure our js file is connected");
+
+//This is a single lined comment.
+//Usually described what comes right after it.
+
+/*
+Multi-lined comment
+anything in between the stars and slashed is ignored by the browsers.
+ */
+
+//Alert-  gives the user information but returns nothing to us.
+alert("Welcome to SDI!");
