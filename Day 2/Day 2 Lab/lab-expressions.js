c55b8b9663a7d2a2e5b2f5e328078b5bfdf69561
NestoLucio <nestolucio@fullsail.edu>
2015-07-10 00:19:58 -0400

    Cleaned up code and made corrections
    Cleaned up code and made corrections

--- Day 2/Day 2 Lab/lab-expressions.js
+++ Day 2/Day 2 Lab/lab-expressions.js
@@ -15,60 +15,63 @@ console.log("Thus be the console.");
 //Day 2 - Lab.
 //Start of variables.
 
-var slices =8;
-var people =16;
-var amountOfPizzas =5;
+var slices = 19;
+var people = 8;
+var amountOfPizzas = 10;
+var total = slices * amountOfPizzas;
 
 //Calculate the number of slice per person.
-var slicesPerPerson = (slices*amountOfPizzas/people);
+var slicesPerPerson = (slices * amountOfPizzas/people);
 
-console.log("Each person ate"+" "+slicesPerPerson+""+"of pizza at the party.");
+console.log("Each person ate"+" "+slicesPerPerson+" "+"of pizza at the party.");
 
 //Number of slices sparky gets to eat.
 //Part II of problem.
 
-var remainder = 40 % 16;
+var remainder = (slices * amountOfPizzas) % people;
 
 console.log("Sparky got"+" "+remainder +" "+"slices of pizza.");
 
 //End of A Piece of the Pie.
 
 //Start of Problem #2 Average Shopping Bill.
-//Weekly gorcery spendings.
+//Weekly grocery spendings.
 
-var week1 =120;
-var week2 =220;
-var week3 =145;
-var week4 =225;
-var week5 =130;
+var week1 = 120;
+var week2 = 220;
+var week3 = 145;
+var week4 = 225;
+var week5 = 130;
 
 //Total amount spent on groceries.
 
-var total =840;
+var total = week1+week2+week3+week4+week5;
 
 //Average grocery spendings.
 
-var average =(week1+week2+week3+week4+week5) /5;
+var average = total /5;
 
 console.log("you have spent a total of"+" "+total+" "+"on groceries over 5 weeks"+ " " +"that is an avarage of"+" "+ average+" "+"per week.");
 
 //Part II of Average shopping bill.
 //Discounts
 
-var originalPrice =25;
-var discount =10;
+//var definitions
+
+var originalPrice = 25;
+var discount = 25;
 var discountPercentage = discount /100;
-var chicken ="lean chicken breast";
-var salesTax =.70;
+var chicken = "lean chicken breast";
+var salesTax = .10;
+var calculateTax = originalPrice * salesTax;
 
-
-//Calculations items.
+//Programmed Calculations items.
 //With tax, without tax, discount + tax, discounts(no tax).
 
-var withTax =originalPrice + salesTax;
-var withoutTax =originalPrice;
+var withTax = originalPrice + salesTax;
+var withoutTax = originalPrice;
 var withDiscountNoTax = originalPrice - (originalPrice * discountPercentage);
-var withDiscountWithTax = originalPrice -(originalPrice * discountPercentage) * salesTax;
+var withDiscountWithTax = originalPrice - (originalPrice * discountPercentage) + calculateTax;
 
 
 console.log("Your"+" "+chicken+" "+"was originally $"+originalPrice+" "+"but after a "+ discount +"% discount, it is now $"+ withDiscountNoTax+" "+"without tax, and $"+withDiscountWithTax+" "+"with tax.");
