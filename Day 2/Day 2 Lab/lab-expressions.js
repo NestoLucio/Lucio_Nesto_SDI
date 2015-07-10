db91c27fce387d4d38e93b810a3bae44cc9dea52
NestoLucio <nestolucio@fullsail.edu>
2015-07-09 22:26:09 -0400

    Calculated Pizza per Person
    Calculated Pizza per Person.

--- Day 2/Day 2 Lab/lab-expressions.js
+++ Day 2/Day 2 Lab/lab-expressions.js
@@ -3,6 +3,25 @@ Created by Nesto Lucio on 7/9/2015.
  SDI Class 00
  Lab 2 Assignment - Expressions.
  */
+
 //alert("js script attached");
+
+alert("SID Lab Assignnment - Lab Expressions");
+
+//Console logging information.
+console.log("Thus be the console.");
+
+//Start of variables.
+
+var slices =8;
+var people =16;
+var amountOfPizzas =5;
+
+//Calculate the number of slice per person.
+var slicesPerPerson = (slices*amountOfPizzas/people);
+
+console.log(slicesPerPerson);
+
+
 
 
