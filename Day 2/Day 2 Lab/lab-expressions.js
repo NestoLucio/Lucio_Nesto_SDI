6d21d70f8b4f2c048d3c555aa40c35ad5f810e67
NestoLucio <nestolucio@fullsail.edu>
2015-07-09 23:05:50 -0400

    Added sales tax
    Added sales tac var to problem 2.

--- Day 2/Day 2 Lab/lab-expressions.js
+++ Day 2/Day 2 Lab/lab-expressions.js
@@ -11,6 +11,8 @@ alert("SID Lab Assignnment - Lab Expressions");
 //Console logging information.
 console.log("Thus be the console.");
 
+//A Piece of the Pie - Problem #1.
+//Day 2 - Lab.
 //Start of variables.
 
 var slices =8;
@@ -20,12 +22,45 @@ var amountOfPizzas =5;
 //Calculate the number of slice per person.
 var slicesPerPerson = (slices*amountOfPizzas/people);
 
-console.log("Each person ate"+" "+slicesPerPerson);
+console.log("Each person ate"+" "+slicesPerPerson+""+"of pizza at the party.");
 
 //Number of slices sparky gets to eat.
 //Part II of problem.
 
 var remainder = 40 % 16;
 
-console.log("Sparky the Dog ate"+" "+remainder +" "+"slices.");
+console.log("Sparky got"+" "+remainder +" "+"slices of pizza.");
+
+//End of A Piece of the Pie.
+
+//Start of Problem #2 Average Shopping Bill.
+//Weekly gorcery spendings.
+
+var week1 =120;
+var week2 =220;
+var week3 =145;
+var week4 =225;
+var week5 =130;
+
+//Total amount spent on groceries.
+
+var total =840;
+
+//Average grocery spendings.
+
+var average =(week1+week2+week3+week4+week5) /5;
+
+console.log("you have spent a total of"+" "+total+" "+"on groceries over 5 weeks"+ " " +"that is an avarage of"+" "+ average+" "+"per week.");
+
+//Part II of Average shopping bill.
+//Discounts
+
+var originalPrice =25;
+var discount =.10;
+var chicken ="lean chicken breast"
+var salesTax =.45
+
+
+
+
 
