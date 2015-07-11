8bec4e898cc310916b13bda0c31c54cabe4df432
NestoLucio <nestolucio@fullsail.edu>
2015-07-11 18:03:07 -0400

    Basic array
    Addition of array elements.

--- Day3/js/day3_arrays.js
+++ Day3/js/day3_arrays.js
@@ -7,3 +7,20 @@ Nesto Lucio
 
 
 //alert("connection established");
+
+//Create a basic array.
+//Picking Oranges.
+
+var orangeBins =[30, 500, 1002];
+
+//How many Oranges did we pick?
+var totalOranges = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log (totalOranges+" Oranges were picked in three days.");
+
+//Average number of Oranges picked
+var averageOranges = totalOranges / orangeBins.length;
+
+//Lenghth property of arrays.
+console.log(orangeBins.length)
+
+console.log ("The average number of Oranges picked is "+ averageOranges);
