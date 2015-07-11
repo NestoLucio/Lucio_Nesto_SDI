7940377c63be750b2c20303486cd709f9b7e09b1
NestoLucio <nestolucio@fullsail.edu>
2015-07-11 19:19:36 -0400

    Covered promps
    Covered promps

--- Day3/js/day3_promps.js
+++ Day3/js/day3_promps.js
@@ -7,3 +7,27 @@ Nesto Lucio
 
 //alert("testing");
 
+//Ask the user for their information.
+var userImput = prompt("Enter your year of birth:");
+console.log(userImput);
+
+var age = 2015-userImput;
+console.log("You are " + age + " years old.")
+
+//Calculate the area of an rectangle.
+
+//Promp the user for width.
+var width = prompt("Lets calculate the area of a rectangle.\nPlease enter a width:");
+
+//Promp the user for height.
+var height = prompt("Please enter a height:");
+
+//Calculate the area using the user input.
+var area = width * height;
+console.log("The area of your rectangle is " + area);
+
+//Create a variable that holds the whole result.
+var result ="The area of your rectangle is " + area;
+console.log(result);
+
+alert(result);
