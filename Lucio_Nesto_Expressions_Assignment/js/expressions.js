81ea277e1ac8a058627bb1cdd0de89cc254ebd42
NestoLucio <nestolucio@fullsail.edu>
2015-07-11 23:18:18 -0400

    Added extra expenses cost
    Added extra expenses cost

--- Lucio_Nesto_Expressions_Assignment/js/expressions.js
+++ Lucio_Nesto_Expressions_Assignment/js/expressions.js
@@ -13,7 +13,7 @@ Nesto Lucio
 //Array - Movies to display set up
 var moviesOut = ["Titanic The Sequel", "Kung-Fu Midgets", "Transforming The Robot Cars", "The Movie -Based on a true story", "2 hours of Movie Trailers"];
 
-console.log(moviesOut);
+console.log (moviesOut);
 
 //Replaced "The Movie - Based on a true story for TED.
 
@@ -21,25 +21,25 @@ moviesOut[3] = "TED";
 console.log(moviesOut);
 
 //pushed "The Hulk" last to the movie list.
-moviesOut.push("The Hulk")
-console.log(moviesOut);
+moviesOut.push ("The Hulk");
+console.log (moviesOut);
 
 
 
 //Movie cost calculations.
 
-var userInputMovies = prompt("Enter movie ticket cost.");
-var userInputAmountOfTickets = prompt("Enter amount of tickets needed.")
+var userInputMovies = prompt ("Enter movie ticket cost.");
+var userInputAmountOfTickets = prompt ("Enter amount of tickets needed.");
 
-console.log ("Your total amount spent on tickets is " + userInputMovies * userInputAmountOfTickets);
+console.log ("Your total amount spent on tickets is $" + userInputMovies * userInputAmountOfTickets);
 
 //Calculating extra spendings.
 
 var popcorn = 4;
 var soda = 3;
 
-var userInputPopcorn = prompt("How many Popcorns will be purchase? Popcorn costs $4.00");
-var userInputSoda = prompt("How many sodas will be purchased? Soda costs $3.00");
+var userInputPopcorn = prompt ("How many Popcorns will be purchase? Popcorn costs $4.00");
+var userInputSoda = prompt ("How many sodas will be purchased? Soda costs $3.00");
 
 
 console.log ("Your total spend on popcorn is $"+ popcorn * userInputPopcorn + " and your total amount on sodas will be $" + soda * userInputSoda);
@@ -49,3 +49,5 @@ console.log ("Your total spend on popcorn is $"+ popcorn * userInputPopcorn + " 
 var totalCost  = (userInputMovies * userInputAmountOfTickets) + (popcorn * userInputPopcorn) + (soda * userInputSoda);
 
 console.log ("Your total overall cost is $" + totalCost);
+
+alert("your total cost is $" + totalCost);
