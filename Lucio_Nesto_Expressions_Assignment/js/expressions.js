21f191d59af779249ee212f650a09b90254ba4a6
NestoLucio <nestolucio@fullsail.edu>
2015-07-11 23:27:50 -0400

    Completed Calculated V1
    Version 1. Completed calculator

--- Lucio_Nesto_Expressions_Assignment/js/expressions.js
+++ Lucio_Nesto_Expressions_Assignment/js/expressions.js
@@ -8,7 +8,7 @@ Nesto Lucio
 //alert("Expressions Assignment");
 
 //Concept idea.
-//How much money on is spend going out to the movies.
+    //How much money on is spend going out to the movies at Nesto's Movie Theatre.
 
 //Array - Movies to display set up
 var moviesOut = ["Titanic The Sequel", "Kung-Fu Midgets", "Transforming The Robot Cars", "The Movie -Based on a true story", "2 hours of Movie Trailers"];
@@ -20,7 +20,7 @@ console.log (moviesOut);
 moviesOut[3] = "TED";
 console.log(moviesOut);
 
-//pushed "The Hulk" last to the movie list.
+//pushed "The Hulk" to the movie list.
 moviesOut.push ("The Hulk");
 console.log (moviesOut);
 
@@ -34,20 +34,18 @@ var userInputAmountOfTickets = prompt ("Enter amount of tickets needed.");
 console.log ("Your total amount spent on tickets is $" + userInputMovies * userInputAmountOfTickets);
 
 //Calculating extra spendings.
-
 var popcorn = 4;
 var soda = 3;
-
-var userInputPopcorn = prompt ("How many Popcorns will be purchase? Popcorn costs $4.00");
-var userInputSoda = prompt ("How many sodas will be purchased? Soda costs $3.00");
-
+var userInputPopcorn = prompt ("How many Popcorn bags will be purchase? Popcorn costs $" + soda);
+var userInputSoda = prompt ("How many sodas will be purchased? Soda costs $" + popcorn);
 
 console.log ("Your total spend on popcorn is $"+ popcorn * userInputPopcorn + " and your total amount on sodas will be $" + soda * userInputSoda);
 
 //Calculating Total cost
-
 var totalCost  = (userInputMovies * userInputAmountOfTickets) + (popcorn * userInputPopcorn) + (soda * userInputSoda);
-
 console.log ("Your total overall cost is $" + totalCost);
 
+//Alert total movie cost.
 alert("your total cost is $" + totalCost);
+
+//This calculator will give you the total amount spend when watching a movie at Nesto's Movie Theatre.
