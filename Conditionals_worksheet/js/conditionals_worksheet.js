345271d6253bf809200d2923ae2318337e0bac5b
NestoLucio <nestolucio@fullsail.edu>
2015-07-14 22:57:16 -0400

    Started Movie Ticket Price
    Started Movie Ticket Price

--- Conditionals_worksheet/js/conditionals_worksheet.js
+++ Conditionals_worksheet/js/conditionals_worksheet.js
@@ -43,16 +43,24 @@ There should be only one print out to the console.
 //Student grades.
 
 var week1Grade = 90;
-var week2Grade = 50;
-var week3Grade = 85;
-var week4Grade = 95;
+var week2Grade = 90;
+var week3Grade = 50;
+var week4Grade = 90;
 
 var gradeAverage = ((week1Grade + week2Grade + week3Grade + week4Grade) / 4);
 console.log ("Total grade average is " + gradeAverage +"%.")
 //
 
-if (gradeAverage >= 80){
-console.log("you have a " + gradeAverage + "%, which means you have earned a B in the class.")
-} else  {
- console.log("you have a " + gradeAverage + "%, which means you have earned a C in the class.")
+if (gradeAverage >= 90){
+console.log("you have a " + gradeAverage + "%, which means you have earned a A in the class.")
+} else {
+ console.log("you have a " + gradeAverage + "%, which means you have earned a B in the class.");
 }
+
+/**
+ *MOVIE TICKET PRICE
+ *The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
+ Determine which of the two prices the customer is eligible for.
+ */
+
+
