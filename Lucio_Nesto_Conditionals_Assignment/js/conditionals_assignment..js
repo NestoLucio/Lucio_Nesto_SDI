6dc10f7a996953ecc6344401a219547c3739b8f1
NestoLucio <nestolucio@fullsail.edu>
2015-07-17 00:10:02 -0400

    added user age prompt
    added user age prompt

--- Lucio_Nesto_Conditionals_Assignment/js/conditionals_assignment..js
+++ Lucio_Nesto_Conditionals_Assignment/js/conditionals_assignment..js
@@ -19,16 +19,30 @@ var windows = 500;
 
 //User Prompt
 
+userAge = prompt("Are you 18 or older?");
+if (isNaN(userAge) || userAge === "" ){
+ //re-prompt user
+ userAge = prompt("Please type in numeric number");
+} else if (userAge <= 18){
+ alert ("Sorry, You're not old enough to use this platform.");
+}
+
 userInput = prompt("are you looking for a Windows or Apple computer?");
  if (userInput.toLowerCase() != "apple" && userInput.toLowerCase() != "windows" ){
      userInput = prompt("Please type in options Apple and Windows only.")
  }
 
-userInputPrice =  prompt("What is your current computer budget?");
+
+userInputPrice =  prompt ("What is your current computer budget?");
  if (isNaN(userInputPrice) || userInputPrice === ""){
   //re-prompt user
    if(isNaN(userInputPrice)) {
     userInputPrice = prompt("Please type in only numeric characters");
    }
  }
-console.log("test");
+
+if (userInput === "apple" && apple >= 1300){
+ console.log("Congrats you can buy an Apple Computer");
+} else if (userInput === "windows" && windows >= 300 ){
+ console.log("Congrats, you can buy a windows computer.");
+}
