7efd768a7e4effcc7bee064d667d61e9bb11d3e8
NestoLucio <nestolucio@fullsail.edu>
2015-07-19 00:38:43 -0400

    Completed While loop
    Completed While loop

--- Lucio_Nesto_Loops/js/while-loops.js
+++ Lucio_Nesto_Loops/js/while-loops.js
@@ -2,11 +2,28 @@
  Nesto Lucio
  Section 00
 7-18-2015
-while Loops
+while Loops - Homework
  */
 
 //alert("testing");
 
 //create saving calculator using while looping.
+
+var  userSavings = prompt("put the amount of money you;d like to save up for 90 days.");
+//validate variable.
+if ((isNaN(userSavings) || userSavings==="")){
+ userSavings = prompt("You can save your words but today we'll save some money\nPlease insert numeric variables only.");
+}
+userSavings=Number(userSavings);
+
+var daysToSave = 90;
+var total =0;
+ while (daysToSave >= 0){
+ daysToSave--;
+  total+=userSavings;
+ }
+console.log("Your total is $" + total);
+
+
 
 
