22a8e87c61c3fa919a6df7aa1943c8f222849d87
NestoLucio <nestolucio@fullsail.edu>
2015-07-18 20:24:24 -0400

    while loop
    while loop

--- Day 6/js/day6_while-loops.js
+++ Day 6/js/day6_while-loops.js
@@ -7,3 +7,30 @@
 
 //alert("testing");
 
+//Initialize a counter veriable.
+
+var counter = 0;
+
+//while loop with condition
+//this will run as long as the condition is true.
+
+while(counter < 200){
+ console.log("The current value is " + counter);
+ //Change the value of the counter var.
+ counter+=5;
+ //Must change the value or it will loop forever.
+}
+
+//do while loop
+//This loop will always run at least one time before checking the condition.
+
+
+//Create counting var.
+
+var i = 0;
+
+do{
+ //This code will always run at least once
+ console.log("the value of i is" + i);
+ i++;
+}while(i>10);
