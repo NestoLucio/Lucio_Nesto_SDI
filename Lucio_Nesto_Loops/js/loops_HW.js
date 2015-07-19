be7e1a90d7c53179423656310fb0b316997d4d70
NestoLucio <nestolucio@fullsail.edu>
2015-07-18 23:40:57 -0400

    for loops
    for loops

--- Lucio_Nesto_Loops/js/loops_HW.js
+++ Lucio_Nesto_Loops/js/loops_HW.js
@@ -1,17 +1,31 @@
-/**
+/*
  Nesto Lucio
  Section 00
 7-18-2015
- While-Loops homework Assignment
+ Loops homework Assignment
  */
 
+//Create savings of calculator
 
-//Create Rent calculator.
-
-//Create variables.
-var rent = prompt("Please insert rent amount");
-//rent validation.
-while(isNaN(rent) || rent ===""){
- rent = ("Please only type in numeric numbers");
+var userDailySavings = prompt("Select dollar amount per day to put into savings so that you can reach the goal of $1000");
+//validation here
+if (isNaN(userDailySavings) || userDailySavings===""){
+ //prompt user to only use numbers
+ userDailySavings = prompt("Please use only numbers");
 }
-console.log("Your rent is currently $" + rent);
+userDailySavings = Number(userDailySavings);
+for( counter=userDailySavings; counter > 0 ; counter += userDailySavings) {
+console.log("Your current saving is $" + counter);
+ if (counter>=1000){
+  //break once user reaches goal os $1000
+  break;
+ }
+}
+console.log("you have reached your goal!");
+
+
+
+
+
+
+
