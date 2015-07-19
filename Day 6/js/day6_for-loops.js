6743136fb581e0a0645193373a0874ceb04b6c20
NestoLucio <nestolucio@fullsail.edu>
2015-07-18 20:37:50 -0400

    for loops
    for loops

--- Day 6/js/day6_for-loops.js
+++ Day 6/js/day6_for-loops.js
@@ -5,5 +5,21 @@
  Topic
  */
 
-alert("testing");
+//alert("testing");
 
+//Basic 4 loop
+//for (initialize counter); condition ; increment of change){}
+
+for (var i = 10 ; i>0 ; i--){
+ console.log("The value of i is" +i);
+}
+
+//Breaks - I this command is run, your loop will stop
+for(var j=0 ; j<5 ; j++){
+ console.log("The value of j is "+ j);
+
+ //If the value of j is 3 stop my loop
+ if (j==3){
+   break;
+ }
+}
