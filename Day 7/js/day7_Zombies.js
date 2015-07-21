380ab75130b3a94222300386bbbbee72c2df1035
NestoLucio <nestolucio@fullsail.edu>
2015-07-21 19:02:04 -0400

    Zombie Apocalypse
    Zombie Apocalypse

--- Day 7/js/day7_Zombies.js
+++ Day 7/js/day7_Zombies.js
@@ -14,14 +14,14 @@
 
 //Create the givens.
     //How many zombies we have.
-/*
+
 var numZombies = 1;
 //Number of people a zombie have per day.
 var numBites = 4;
 
 //What is the time line.
 var days=8;
-
+/*
 for (var i=1; i<=days; i++){
  //How many people get infected every day
  var infectedPeople = numZombies*numBites;
@@ -31,3 +31,20 @@ for (var i=1; i<=days; i++){
 
 }
 */
+
+//The CDC wants to know how many days it will take would it take for there to be a million zombies.
+
+//variable to count the number of days.
+var numDays = 1;
+
+while(numZombies<=100000){
+ //How many people get infected every day
+ var infectedPeople = numZombies*numBites;
+ //How many Zombies will we have.
+ numZombies+=infectedPeople;
+ //Report out each day
+ console.log("On day number "+numDays+" there will be "+numZombies+ " Zombies!")
+//increase
+ numDays++;
+}
+console.log("It will take "+ (numDays-1) + " days for a million zombies");
