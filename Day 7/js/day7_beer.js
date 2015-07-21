4600984c621d7b786e9fdfd1ded667f120219f5f
NestoLucio <nestolucio@fullsail.edu>
2015-07-21 18:18:40 -0400

    99 bottles, part 2
    99 bottles, part 2

--- Day 7/js/day7_beer.js
+++ Day 7/js/day7_beer.js
@@ -11,15 +11,26 @@
 
 //for (initinalizeation; condition; incrememnt of change){}
 
-for(var bottles=99; bottles>0; bottles--) {
+var numBeers= prompt("How many beers are on your wall?");
+//Validations
+while (isNaN(numBeers) || numBeers===""){
+ if (isNaN(numBeers)){
+  numBeers=prompt("Please type in number\nHow many beers are on your wall?");
+ }else{
+  numBeers =prompt("okease di bit keave blank\nHow many beers are on your wall?")
+ }
+}
+//cast the variable
+numBeers=Number(numBeers);
+for(var bottles=numBeers; bottles>0; bottles--) {
  //verify that the loop works.
  //console.log(bottles);
  if (bottles===2) {
-  console.log(bottles + " bottles of beer on the wall. " + bottles + " of bottles beer. You take one down and pass it around." + (bottles - 1) + " Bottle of beer on the wall");
- }else if(bottles===1){
-  console.log(bottles + " bottle of beer on the wall. " + bottles + " of bottle beer. You take one down and pass it around." +  " The End.");
+  console.log(numBeers+ " bottles of beer on the wall. " + numBeers + " of bottles beer. You take one down and pass it around." + (numBeers - 1) + " Bottle of beer on the wall");
+ }else if(numBeers===1){
+  console.log(numBeers + " bottle of beer on the wall. " + numBeers + " of bottle beer. You take one down and pass it around." +  " The End.");
 
  }else{
-  console.log(bottles + " bottles of beer on the wall. " + bottles + " of bottles beer. You take one down and pass it around." + (bottles - 1) + " Bottles of beer on the wall");
+  console.log(numBeers + " bottles of beer on the wall. " + numBeers + " of bottles beer. You take one down and pass it around." + (numBeers - 1) + " Bottles of beer on the wall");
  }
 }
