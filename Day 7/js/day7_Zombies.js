60c9d66b5a4bd9ae2075eb2aafec1f86cf04ee17
NestoLucio <nestolucio@fullsail.edu>
2015-07-21 18:48:57 -0400

    For loop version for 8 days
    For loop version for 8 days

--- Day 7/js/day7_Zombies.js
+++ Day 7/js/day7_Zombies.js
@@ -7,3 +7,27 @@
 
 //alert("testing");
 
+//We have 1 zombie here at Full Sail
+//it can bite 4 people a day and turn them into Zombies in 24 hours.
+
+//The CDC wants to know how many Zombies there will be in 8 days.
+
+//Create the givens.
+    //How many zombies we have.
+/*
+var numZombies = 1;
+//Number of people a zombie have per day.
+var numBites = 4;
+
+//What is the time line.
+var days=8;
+
+for (var i=1; i<=days; i++){
+ //How many people get infected every day
+ var infectedPeople = numZombies*numBites;
+ //How many Zombies will we have.
+ numZombies+=infectedPeople;
+ console.log("On day number "+i+" there will be "+numZombies+ " Zombies!")
+
+}
+*/
