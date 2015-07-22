609ecce3d025f62e1ac98148f4d87f371a60a473
NestoLucio <nestolucio@fullsail.edu>
2015-07-21 21:49:16 -0400

    setting up loops
    v

--- Lucio_Nesto_Array_Loop/js/ArrayLoop_worksheet.js
+++ Lucio_Nesto_Array_Loop/js/ArrayLoop_worksheet.js
@@ -5,6 +5,26 @@
  ArrayLoop worksheet
  */
 
-alert("testing");
+//alert("testing");
 
 
+//Create prompts based on the Justice League.
+
+var avengers =["Iron Man","The Hulk","Captain America","Black Widow","Hawkeye"];
+var fightAvengers = prompt("Choose what member from the Justice League you will be fighting. " + avengers);
+//console.log(fightAvengers);
+
+//Validate user prompt.
+/*while (fightAvengers.toLowerCase() || fightAvenger.toLowerCase()===""){
+//re-prompt user
+ fightAvengers = prompt("Villain DataBase only has information on The Avengers Listed Below " + "Which Avenger will you be bringing down? "+ avengers);
+}
+ */
+
+//Create villain weakness.
+//Cycle through Avengers and show weakness.
+
+for (var i=0; i<avengers.length; i++){
+ //Set up text hero text strings and call-out weakness.
+ 
+}
