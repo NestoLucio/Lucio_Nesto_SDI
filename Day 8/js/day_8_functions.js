7b706e4a01c80e8a99b9831c34caa5379eacef5a
NestoLucio <nestolucio@fullsail.edu>
2015-07-23 17:55:45 -0400

    Learning Functions
    Learning Functions

--- Day 8/js/day_8_functions.js
+++ Day 8/js/day_8_functions.js
@@ -5,5 +5,45 @@
 Functions
  */
 
-alert("testing");
+calcArea();
+//alert("testing");
 
+//Basic Function Structure
+// function functionName(){Code to run goes here}
+
+printHello();
+//Create a function that will console log something.
+function printHello(){
+//This code will run when the function run or is called upon
+ console.log("This Function is working");
+
+}
+
+//Create a function call that will start our printHello function.
+//Call the function by using the name.
+printHello();
+printMore();
+printHello();
+
+//Create a function that console logs a different string.
+function printMore (){
+ console.log("This is different text");
+}
+
+//function call printMore
+printMore();
+
+//Create a function that calculates the area of a rectangle.
+
+function calcArea (){
+ //Create variables for W,H,A
+
+ var width=20;
+ var height=30;
+ var area=width*height;
+ console.log("The area of the recatangle is " + area + "sq Feet");
+}
+
+//Create the function call
+
+calcArea();
