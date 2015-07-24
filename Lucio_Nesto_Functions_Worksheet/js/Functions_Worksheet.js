40d8587f7f30fbf5c0592144f5c690a203c9afca
NestoLucio <nestolucio@fullsail.edu>
2015-07-23 20:41:37 -0400

    Tested circumference funtion
    Tested circumference funtion - adding perameters and arguments

--- Lucio_Nesto_Functions_Worksheet/js/Functions_Worksheet.js
+++ Lucio_Nesto_Functions_Worksheet/js/Functions_Worksheet.js
@@ -5,5 +5,21 @@
 Functions_Worksheet
  */
 
-alert("testing");
+//alert("testing");
 
+//Calculate the circumference of a circle.
+//Create a funtion
+
+
+function calcCircle(r){
+
+        //calculate the circumference with the radius (circumference = C=2πr)
+        var diameter =50;
+        var circumTotal = (2*Math.PI) * r;
+        //test variables.
+        console.log("The circumference of the circle is " + circumTotal);
+}
+
+//Call out function
+//And give it an argument.
+calcCircle(50);
