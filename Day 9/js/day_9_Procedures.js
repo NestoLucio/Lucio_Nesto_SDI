e68f1c11dcc35b190a99f7b90a90754ec8658105
NestoLucio <nestolucio@fullsail.edu>
2015-07-25 19:00:43 -0400

    Learned Procedures
    Learner about Procedures

--- Day 9/js/day_9_Procedures.js
+++ Day 9/js/day_9_Procedures.js
@@ -5,5 +5,29 @@
  day_9_Procedures
  */
 
-alert("testing");
+//alert("testing");
+//this is a normanl function
+ function calcArea(w, h){
+         var area = w*h;
+         //REturns a variable
+         return area;
+}
 
+//Function call to calc area
+//calcArea(10,20);
+//cacth the retunr value
+var results1 = calcArea(10,20);
+console.log(results1);
+
+
+//THIS IS A PROCEDURE
+//A list of steps to do
+//Returns NOTHING.
+
+function calcPeri(w,h){
+       var peri = 2*w +2*h;
+       console.log(peri);
+}
+//Function call for ou procedure
+//This function call does not catch anything
+calcPeri(10,20);
