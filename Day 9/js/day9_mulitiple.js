13f67013b56b87668cf1afc0bcf611b77d0d930f
NestoLucio <nestolucio@fullsail.edu>
2015-07-25 19:59:38 -0400

    Testing if we should build a function or not
    Testing if we should build a function or not

--- Day 9/js/day9_mulitiple.js
+++ Day 9/js/day9_mulitiple.js
@@ -8,7 +8,7 @@
 //alert("testing");
 
 //Create variables for height and width.
- var hight = 8;
+ var height = 8;
  var width = 6;
 
 //Create another variable for a base of triangle.
@@ -17,7 +17,7 @@ var base = 5;
 
 
 //function call to area
-var resultsArea = calcArea(hight,width);
+var resultsArea = calcArea(height,width);
 console.log(resultsArea);
 
 
@@ -28,7 +28,7 @@ var calcPeri = function (h, w){
 };
 
 //Function call must go under the function definition for anon.
-var resultsPeri = calcPeri(hight, width);
+var resultsPeri = calcPeri(height, width);
 console.log (resultsPeri);
 
 //Create a function to calc the area of a trinagle.
@@ -38,17 +38,29 @@ function calcArea(h, w){
 }
 
 //Create a function to calculate the area of triangle.
-//create a anon function
 
-var triArea = function (b, h){
+
+//Ask user if they would like to clac the are of a triangle.
+var userPrompt = prompt("Would you like to calculate the are of a triangle");
+while(userPrompt.toLowerCase() !="yes" && userPrompt.toLowerCase() !="no" ){
+      userPrompt = prompt("Please only answer yes or no\nWould you like to calc the area of a triangle.");
+}
+
+//Conditional to see if we should run the anon function.
+if (userPrompt.toLowerCase() ==="yes"){
+      var triArea = function (b, h){
       var area = .5*b*h;
       return area
-};
+ };
 
-//Function call to triangle area
+      //Function call to triangle area
 
-var triAreaTotal = triArea(base, hight);
-console.log(triAreaTotal);
+      var triAreaTotal = triArea(base, height);
+      console.log(triAreaTotal);
+
+}
+
+
 
 //Create a procedure that will console log the rectangle perimeter and area.
 
